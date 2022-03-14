59fff65db708196c6ea89a32564accb4f85c1cc1
Simo Tumelius <simo@datamie.fi>
2022-03-14 12:12:47 +0200

    Add lab 5 correct answers

--- models/orders.sql
+++ models/orders.sql
@@ -31,9 +31,11 @@ joined as (
         orders.order_status,
         orders.total_amount,
         orders.store_id,
+        stores.store_name,
         datediff('minutes', orders.ordered_at, deliveries_filtered.delivered_at) as delivery_time_from_order,
         datediff('minutes', deliveries_filtered.picked_up_at, deliveries_filtered.delivered_at) as delivery_time_from_collection
     from orders
+    left join stores on (orders.store_id = stores.store_id)
     left join deliveries_filtered on (orders.order_id = deliveries_filtered.order_id)
     left join customers on (orders.customer_id = customers.customer_id)
     where customers.email not ilike '%ecommerce.com'
