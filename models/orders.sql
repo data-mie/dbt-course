e7509bf335e33aea3cd37d181e1ec9edb57d6fbd
Simo Tumelius <simo@datamie.fi>
2022-03-14 12:39:31 +0200

    Add lab 8 correct answers

--- models/orders.sql
+++ models/orders.sql
@@ -1,7 +1,17 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='order_id'
+    )
+}}
+
 with orders as (
     select
         *
     from {{ ref('stg_ecomm__orders') }}
+    {% if is_incremental() %}
+        where ordered_at > (select dateadd('day', -3, max(ordered_at)) from {{ this }})
+    {% endif %}
 ),
 
 deliveries as (
