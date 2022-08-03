0141050c374f0ac9aaa44c5e1275bda024182b2b
Simo Tumelius <simo@datamie.fi>
2022-08-03 11:27:19 +0300

    Change stg_ecomm__orders now unions orders_us, orders_de and orders_au tables

--- models/stg_ecomm__orders.sql
+++ models/stg_ecomm__orders.sql
@@ -1,18 +1,40 @@
-with source as (
+with sources as (
+    {{
+        dbt_utils.union_relations(
+            relations=[
+                source('ecomm', 'orders_us'),
+                source('ecomm', 'orders_de'),
+                source('ecomm', 'orders_au')
+            ],
+        )
+    }}
+),
+
+store_codes as (
     select
-        *
-    from {{ source('ecomm', 'orders') }}
+        *,
+        split_part(split_part(_dbt_source_relation, '.', 3), '_', 2) as store_code
+    from sources
+),
+
+store_ids as (
+    select
+        *,
+        case
+            when store_code = 'us' then 1
+            when store_code = 'de' then 2
+            when store_code = 'au' then 3
+        end as store_id
+    from store_codes
 ),
 
 fields as (
     select
+        *,
         id as order_id,
-        customer_id,
         created_at as ordered_at,
-        status as order_status,
-        store_id,
-        total_amount
-    from source
+        status as order_status
+    from store_ids
 )
 
 select
