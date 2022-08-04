b85f61f6cd85199fdccb2b1795c9e14ec0be2c40
Simo Tumelius <simo@datamie.fi>
2022-08-04 13:51:18 +0300

    Add int_ecomm__orders_enriched

---
+++ models/int_ecomm__orders_enriched.sql
@@ -0,0 +1,36 @@
+with orders as (
+  select
+    *
+  from {{ ref('stg_ecomm__orders') }}
+),
+
+rates as (
+    select
+        *
+    from {{ ref('stg_finance__conversion_rates_usd') }}
+),
+
+order_rates as (
+  select
+      orders.*,
+      case
+        when orders.store_code = 'us' then 1
+        else rates.rate_usd
+      end as rate_usd
+  from orders
+  left join rates on (
+    orders.created_at::date = rates.date_day
+    and orders.currency = rates.currency
+  )
+),
+
+final as (
+    select
+        *,
+        total_amount * rate_usd as total_amount_usd
+    from order_rates
+)
+
+select
+    *
+from final
