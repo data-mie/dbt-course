c7b68e16681d40f8885c2314d0e66c3ef9c08e03
Simo Tumelius <simo.tumelius@gmail.com>
2024-04-08 14:09:14 +0300

    Revert "Replace cluster_by exercise with clone+dynamic_table exercise in Lab …" (#15)
    This reverts commit 4162709ebe12b6d406bdaaf11380e253a0d5bbff.

--- models/calendar.sql
+++
@@ -1,9 +0,0 @@
-{% set max_date = '2022-03-14' %}
-{% if execute %}
-    {% set results = run_query('select current_date()') %}
-    {% set max_date = results.columns[0].values()[0] %}
-{% endif %}
-
-{{ log("Calendar max date: " ~ max_date) }}
-
-{{ dbt_date.get_date_dimension('2020-01-01', max_date) }}
